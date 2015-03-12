1f27cf4050f3394dcf2dcf41c1b72b0948d917a0
Vincent <moittie.vincent@gmail.com>
2015-03-12 18:04:08 +0100

    Ajout Sign et de la Value d'une carte, fin setvalue d'une carte.

--- initial/src/main/java/blackjack/model/Card.java
+++ initial/src/main/java/blackjack/model/Card.java
@@ -6,25 +6,43 @@ package blackjack.model;
  *
  * */
 public class Card {
-    private String sign ;
-    private String value ;
+    private SignOfCard sign ;
+    private ValueOfCard value ;
     private int score ;
 
-
-    public String getSign() {
-        return sign;
+    public Card(SignOfCard sign, ValueOfCard value){
+        setSign(sign);
+        setValue(value);
     }
 
-    public void setSign(String sign) {
+    public String getSign() {
+        return sign.name();
+    }
+
+    public void setSign(SignOfCard sign) {
         this.sign = sign;
     }
 
     public String getValue() {
-        return value;
+        return value.name();
     }
 
-    public void setValue(String value) {
+    public void setValue(ValueOfCard value) {
         this.value = value;
+        //TODO à améliorer
+        if(ValueOfCard.V2.equals(value)){ setScore(2); } else
+        if(ValueOfCard.V3.equals(value)){ setScore(3); } else
+        if(ValueOfCard.V4.equals(value)){ setScore(4); } else
+        if(ValueOfCard.V5.equals(value)){ setScore(5); } else
+        if(ValueOfCard.V6.equals(value)){ setScore(6); } else
+        if(ValueOfCard.V7.equals(value)){ setScore(7); } else
+        if(ValueOfCard.V8.equals(value)){ setScore(8); } else
+        if(ValueOfCard.V9.equals(value)){ setScore(9); } else
+        if(ValueOfCard.V10.equals(value)){ setScore(10); } else
+        if(ValueOfCard.Valet.equals(value)){ setScore(10); } else
+        if(ValueOfCard.Queen.equals(value)){ setScore(10); } else
+        if(ValueOfCard.King.equals(value)){ setScore(10); } else
+        if(ValueOfCard.As.equals(value)){ setScore(1); }
     }
 
     public int getScore() {
---
+++ initial/src/main/java/blackjack/model/SignOfCard.java
@@ -0,0 +1,11 @@
+package blackjack.model;
+
+/**
+ * Created by root on 12/03/15.
+ */
+public enum SignOfCard {
+    Heart,
+    Diamond,
+    Club,
+    Spade
+}
